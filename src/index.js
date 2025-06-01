871072278098b089841184f49c6e816988079c8b
HARSH KUMAR <xt.harshh@gmail.com>
2025-06-01 20:34:18 +0530

    new

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { connectDB } from "./src/db/index.js";
-import app from "./src/app.js";
+import { connectDB } from "./db/index.js";
+import app from "./app.js";
 import http from 'http'
 const server = http.createServer(app)
 import express from 'express';
